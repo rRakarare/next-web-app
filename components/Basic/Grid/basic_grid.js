9e2ef5db12bdd7566f14ccbca430e092aa1ddbb5
rRakarare <rRene333@googlemail.com>
2021-12-07 22:23:42 +0100

    grid_start

---
+++ components/Basic/Grid/basic_grid.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Grid_2 = styled.div`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  grid-gap: 10px;
+  grid-auto-rows: minmax(100px, auto);
+`;
